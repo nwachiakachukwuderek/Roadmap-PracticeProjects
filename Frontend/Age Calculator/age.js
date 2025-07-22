83eacd68d860dbe162ad420452df65beb020471f
TheGreatMe <nwachiakachukwu5@gmail.com>
2025-07-22 11:11:58 +0100

    started creating a age calculator

---
+++ Frontend/Age Calculator/age.js
@@ -0,0 +1,8 @@
+  const input = document.querySelector('input');
+  const button = document.querySelector('button');
+
+  function date() {
+    let inputValue = input.value
+  }
+
+  button.addEventListener('click', date)
