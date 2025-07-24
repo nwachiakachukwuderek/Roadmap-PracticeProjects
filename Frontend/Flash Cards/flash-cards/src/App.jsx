1868efcd49930ddf520a0b979813965f5ec300a9
TheGreatMe <nwachiakachukwu5@gmail.com>
2025-07-24 14:30:50 +0100

    created the first step to what it should look like

--- Frontend/Flash Cards/flash-cards/src/App.jsx
+++ Frontend/Flash Cards/flash-cards/src/App.jsx
@@ -7,6 +7,22 @@ function App() {
   return (
     <>
       <h1>Flash Cards</h1>
+      <div className="percentage-bar">
+        <span className="remaining">0 </span>
+        <span>of 20</span>
+      </div>
+
+      <div>
+        <div className="ques-ans">
+          What is the difference between var, let and const?
+        </div>
+
+        <div>
+          <span>Previous</span>
+          <span>Show Answer</span>
+          <span>Next</span>
+        </div>
+      </div>
     </>
   )
 }
